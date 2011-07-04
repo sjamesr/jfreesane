aea9eba34174504599ad84229d8c4b8a647dff05
sjr <sjr@google.com>
2011-07-03 21:53:45 -0700

    Add helper methods for dealing with bit vectors

--- src/main/java/au/com/southsky/jfreesane/SaneEnums.java
+++ src/main/java/au/com/southsky/jfreesane/SaneEnums.java
@@ -1,9 +1,13 @@
 package au.com.southsky.jfreesane;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 
 /**
  * Utilities for dealing with instances of {@link SaneEnum}.
@@ -34,6 +38,41 @@ public class SaneEnums {
 		Map<Integer, T> result = mapBuilder.build();
 
 		cachedTypeMaps.put(enumType, result);
+		return result;
+	}
+
+	/**
+	 * Returns a set of {@code T} obtained by treating {@code wireValue} as a
+	 * bit vector whose bits represent the wire values of the enum constants of
+	 * the given {@code enumType}.
+	 */
+	public static <T extends Enum<T> & SaneEnum> Set<T> enumSet(
+			Class<T> enumType, int wireValue) {
+		T[] enumConstants = enumType.getEnumConstants();
+		List<T> values = Lists.newArrayListWithCapacity(enumConstants.length);
+
+		for (T value : enumType.getEnumConstants()) {
+			if ((wireValue & value.getWireValue()) != 0) {
+				values.add(value);
+			}
+		}
+
+		return Sets.immutableEnumSet(values);
+	}
+
+	/**
+	 * Returns the result of bitwise-ORing the wire values of the given
+	 * {@code SaneEnum} set. This method does not check to make sure the result
+	 * is sensible: the caller must ensure that the set contains members whose
+	 * wire values can be ORed together in a logically correct fashion.
+	 */
+	public static <T extends SaneEnum> int wireValue(Set<T> values) {
+		int result = 0;
+
+		for (T value : values) {
+			result |= value.getWireValue();
+		}
+
 		return result;
 	}
 
